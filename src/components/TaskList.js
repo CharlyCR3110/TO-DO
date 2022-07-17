bc5b9b804cfbea4b933239f3b8ee5ce58aab6113
charly <carlosgarita3110@gmail.com>
2022-07-17 16:51:19 -0600

    Add task function

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import TaskForm from './TaskForm';
+import Task from './Task';
 import '../stylesheets/TaskList.css';
+import { useState } from 'react';
+
+const addTask = task => {
+	console.log('Sexo');
+	console.log(task);
+};
 
 function TaskList () {
+
+	const [tasks, setTask] = useState([]);
+
   return (
 		<>
 			<TaskForm />
 			<div className='taskListContainer'>
+				{
+					tasks.map((task) =>
+						<Task 
+							text={task.text}
+							completed={task.completed}
+						/>
+					)
+				}
 			</div>
 		</>
   );
