d6bc8b6dc348991ca0817dfd7ae263452521dee8
charly <carlosgarita3110@gmail.com>
2022-07-17 16:49:32 -0600

    add a change handler

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useState } from 'react';
 import '../stylesheets/TaskForm.css';
 
 function TaskForm (props) {
+
+	const [input, setInput] = useState('')
+
+	const changeHandler = e => {
+		setInput(e.target.value);
+		console.log(e.target.value);
+	}
+	
 	return (
 		<form className='taskForm'>
 			<input
+				onChange={changeHandler}
 				className='taskInput'
 				type='text'
 				placeholder='Write a task'
