9a91d0c61afe1062581a48de77e2193e8006b24e
charly <carlosgarita3110@gmail.com>
2022-07-17 16:50:49 -0600

    event listeners for delete and complete tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,13 +2,17 @@ import React from 'react';
 import '../stylesheets/Task.css';
 import { AiOutlineCloseCircle } from 'react-icons/ai';
 
-function Task({ texto, completed }) {
+function Task({ id, texto, completed, completeTask, deleteTask}) {
 	return (
 		<div className={completed ? 'taskContainer completed': 'taskContainer'}>
-			<div className='text'>
+			<div 
+				className='text'
+				onClick={() => completeTask(id)}>
 				{texto}
 			</div>
-			<div className='taskIconsContainer'>
+			<div 
+				className='taskIconsContainer'
+				onClick={() => deleteTask(id)}>
 				<AiOutlineCloseCircle className='taskIcon'/>
 			</div>
 		</div>
